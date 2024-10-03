09d75b37fc364317505a927883314174f13ce192
ahka07001 <ahka07001@E-PW05CGQT>
2024-10-03 13:44:06 +0200

    Add project files.

---
+++ Inlämning uppgift 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inlämning_._uppgift_._2_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favortibok?");
+            string favoritbok= Console.ReadLine();
+
+            Console.WriteLine(favoritbok);
+        }
+    }
+
+}
